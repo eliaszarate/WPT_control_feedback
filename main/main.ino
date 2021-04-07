2fdeee10f8f55edbc6d63b0f82287d92b82f64ae
RebecaLopezValerio <rebecalopez11118@gmail.com>
2021-04-07 13:17:51 -0700

    adding changes

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,15 @@
+byte PWM_PIN = 3;
+int pwm_value;
+int freq;
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(PWM_PIN, INPUT);
+  Serial.begin(115200);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  pwm_value = pulseIn(PWM_PIN, HIGH);
+  freq = (1/(pwm_value * pow(10, -6)))/2;
+  // Should show 2.5khz
+  Serial.println(freq);
+  voltageOut = map(freq, 1600, 2500, 255, 0);
+ // analogWrite(pinOut,voltageOut) //output voltage on pinOut from 0V to 5V. 
 }
