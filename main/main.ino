0c941a68ded9417f9a0ab51ddfa51abfa0df1836
Elias <elias@Eliass-MacBook-Pro.local>
2021-04-07 17:55:54 -0700

    Comments for test

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,39 @@
+
+/*
+ * Arduino .ino file for DSP
+ * feedback testing
+ */
+
 byte PWM_PIN = 3;
+byte pinOut = 10;
 int pwm_value;
+int voltageOut;
 int freq;
+
 void setup() {
   pinMode(PWM_PIN, INPUT);
   Serial.begin(115200);
 }
+
 void loop() {
+  // Calculate frequency from PWM input
   pwm_value = pulseIn(PWM_PIN, HIGH);
   freq = (1/(pwm_value * pow(10, -6)))/2;
-  // Should show 2.5khz
+  Serial.print(F("Freq: "));
   Serial.println(freq);
+
+  // Saturate, avoid negative values
+  if(freq < 1600){
+    freq = 1600;
+  } else if (freq > 2500){
+    freq = 2500;    
+  }
+
+  // Map voltage ouput with input frequency
   voltageOut = map(freq, 1600, 2500, 255, 0);
+  Serial.print(F("Voltage: "));
+  Serial.println(voltageOut);
+  
+ // Test to make sure voltage is correct
  // analogWrite(pinOut,voltageOut) //output voltage on pinOut from 0V to 5V. 
 }
